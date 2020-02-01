ec2d5337a0a84c30e2076b4ea38867b8405bea23
Kenneth Pham <kennethpham98@gmail.com>
2020-02-01 00:38:31 -0800

    added react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Homepage from './components/Homepage.js'
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Homepage extends React.Component {
+
+
+    render() {
+
+        return(
+
+            <div>
+                Homepage
+            </div>
+        );
+
+
+    }
+
+};
+
+export default Homepage;
